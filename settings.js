2b7ee4945bf2a329f1944f155620252eede22445
Ay12thehero <Ay12thehero@gmail.com>
2023-07-03 16:15:54 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
+global.owner = ['249124722435'] //ur owner number
+global.ownername = "MISKOL 3MK❤️" //ur owner name
+global.ytname = "YT: MISKOL" //ur yt chanel name
+global.socialm = "GitHub: MISKOL " //ur github or insta name
+global.location = "Africa, Sudan, Khartoum" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
+global.ownernomer = "249124722435" //ur number
+global.premium = ['249124722435'] //ur premium number
+global.botname = '❤️MISKOL ᗷOT❤️' //ur bot name
+global.linkz = "https://youtube.com/@Miskol" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.botscript = 'https://youtube.com/@miskol' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣ميسكول الوزير يروحي⍣" //ur sticker watermark packname
+global.author = "⍣0124722435⍣" //ur sticker watermark author
+global.wm = "❤️MISKOL ᗷOT❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
